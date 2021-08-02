1566d3bd4bb903120ba075f6c4f5d273ba68219a
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-08-02 15:54:47 -0400

    Add initial script

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,33 @@
-import { printLine } from './modules/print';
+setInterval(replaceItems, 3000);
+setTimeout(replaceTaboolaImage, 3000);
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+function replaceItems() {
+  replaceIframeAds();
+}
 
-printLine("Using the 'printLine' function from the Print Module");
+function replaceIframeAds() {
+  document.querySelectorAll('body iframe').forEach((elem) => {
+    const height = +elem.height;
+    const width = +elem.width;
+
+    replace(elem, height, width);
+  });
+}
+
+function replaceTaboolaImage() {
+  document
+    .querySelectorAll('body .trc_elastic_thumbnails-c .videoCube')
+    .forEach((elem) => {
+      replace(elem.querySelector('a'), elem.clientWidth, elem.clientWidth);
+    });
+}
+
+function replace(elem, height, width) {
+  if (height && width) {
+    const image = document.createElement('img');
+    image.src = `https://via.placeholder.com/${width}x${height}`;
+    elem.parentNode.replaceChild(image, elem);
+  } else {
+    elem.parentNode.removeChild(elem);
+  }
+}
